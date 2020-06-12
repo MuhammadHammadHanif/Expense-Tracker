56051caee84314d9590d55b2cff0181021392656
Muhammad Hammad <hammadhanif0@gmail.com>
2020-06-12 13:35:01 +0500

    Create Components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import './App.css';
+
+import { Header } from './components/Header';
+import { Balance } from './components/Balance';
+import { IncomeExpenses } from './components/IncomeExpenses';
+import { TransactionList } from './components/TransactionList';
+import { AddTransaction } from './components/AddTransaction';
 
 function App() {
   return (
-    <div>
-      <h1>React</h1>
-    </div>
+    <Fragment>
+      <Header title='Expense Tracker' />
+      <div className='container'>
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+        <AddTransaction />
+      </div>
+    </Fragment>
   );
 }
 
